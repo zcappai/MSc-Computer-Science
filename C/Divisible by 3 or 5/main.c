fa4254f8a113325447d889dfc556bae9b4580989
Akhilesh Pai <56542660+zcappai@users.noreply.github.com>
2020-12-22 15:38:03 +0000

    Updating comments

--- C/Divisible by 3 or 5/main.c
+++ C/Divisible by 3 or 5/main.c
@@ -2,11 +2,11 @@
 #include <stdbool.h>
 
 /*
- * Write a function which returns true if a number is divisible by 3 and  
- * false if it is not.  Hint: use the modulo (%) operator.
+ * The 1st function below returns true if a number is divisible by 3 and  
+ * false if it is not, using the modulo (%) operator.
  *
- * Modify your code so that your function now returns true if a number is divisible 
- * by 3 or 5 and false if it is not.
+ * The 2nd function below returns true if a number is divisible by 3 or 5
+ * and false if it is not.
  *
 */
 
